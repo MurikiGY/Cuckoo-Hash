35ecc10bd6f01b1a6a0bfa1bc767ffe5e0da9869
Muriki <mgy20@inf.ufpr.br>
2022-09-06 22:07:30 -0300

    Tests OK

--- hash.c
+++ hash.c
@@ -18,20 +18,62 @@ int hash2(int k){
     return floor(M*(k*0.9 - floor(k*0.9)));
 }
 
-void insere_hash(hash_t T1[], hash_t T2[], int k){
-    int index1 = hash1(k);
+void busca_hash(hash_t T1[], hash_t T2[], int k, int *T, int *pos){
+    int index = hash1(k);
 
-    if (T1[index1].status != CHEIO)
-    {
-	T1[index1].chave = k;
-	T1[index1].status = CHEIO;
+    //Testa T1 vazio
+    if (T1[index].status == VAZIO){
+
+        *T = 0;
+        *pos = -1;
+
+       //Testa T1 cheio
+    } else if (T1[index].status == CHEIO && T1[index].chave == k){
+
+        *T = 1;
+        *pos = index;
+
+    } else {
+
+    //T1 excluido ou cheio e sem a chave
+        index = hash2(k);
+    //Testa T2
+        if (T2[index].status == CHEIO && T2[index].chave == k){
+            *T = 2;
+            *pos = index;
+        } else {
+            *T = 0;
+            *pos = -1;
+        }
+
     }
-    else if (T1[index1].chave != k)
-    {
-        int i = hash2(T1[index1].chave);
-        T2[i].chave = T1[index1].chave;   //copia de T1 para T2
-        T2[i].status = CHEIO;
-        T1[index1].chave = k;
+}
+
+void insere_hash(hash_t T1[], hash_t T2[], int k){
+    int T, index;
+
+    //Busca chave duplicada
+    busca_hash(T1, T2, k, &T, &index);
+
+    //Se não ha chave duplicada
+    if (!T){
+        index = hash1(k);
+        if (T1[index].status != CHEIO){
+
+            T1[index].chave = k;
+            T1[index].status =  CHEIO;
+
+        } else {
+
+            int index2 = hash2(T1[index].chave);
+            if (T2[index2].status != CHEIO){
+
+                T2[index2].chave = T1[index].chave;
+                T2[index2].status = CHEIO;
+                T1[index].chave = k;
+
+            }
+        }
     }
 }
 
@@ -60,32 +102,6 @@ void remove_hash_busca(hash_t T1[], hash_t T2[], int k){
         else
         if (T == 2)
             T2[index].status = EXCLUIDO;
-}
-
-void busca_hash(hash_t T1[], hash_t T2[], int k, int *T, int *pos){
-    int index = hash1(k);
-
-    //Testa T1 vazio
-    if (T1[index].status == VAZIO){
-        *T = 0;
-        *pos = -1;
-	   //Testa T1 cheio
-    } else if (T1[index].status == CHEIO && T1[index].chave == k){
-        *T = 1;
-        *pos = index;
-    } else {
-	//T1 excluido ou cheio e sem a chave
-        index = hash2(k);
-	//Testa T2
-        if (T2[index].status == CHEIO && T2[index].chave == k){
-            *T = 2;
-            *pos = index;
-        } else {
-            *T = 0;
-            *pos = -1;
-        }
-    }
-
 }
 
 void imprime_hash(hash_t T1[], hash_t T2[]){
